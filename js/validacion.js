5e7286a13da1bdc84f8451e6de40786965b60171
ximenap1205 <xparedes40@gmail.com>
2024-08-15 20:51:19 -0300

    validacion password

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,38 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let nombre = document.getElementById ("nombre")
+nombre.value === '' 
+
+let apellido = document.getElementById ("apellido")
+apellido.value ==='' 
+
+let password1 = document.getElementById ("password1")
+password1.value ==='' 
+
+let password2 = document.getElementById ("password2")
+password2.value ==='' 
+
+let email = document.getElementById ("email")
+email.value ==='' 
+
+let terminos = document.getElementById ("terminos")
+terminos.value ==='' 
+
+let button = document.getElementById("regBtn")
+
+function Registro() {
+    if (nombre.value !== '' && apellido.value !== '' && password1.value !== '' && password2.value !== '' && email.value !== '' && terminos.checked) {
+        if (password1.value === password2.value && password1.value.length >= 6) {
+            showAlertSuccess();
+        } else {
+            showAlertError();
+        }
+    } else {
+        showAlertError();
+    }
+} 
+
+document.getElementById("regBtn").addEventListener('click', Registro);
+
